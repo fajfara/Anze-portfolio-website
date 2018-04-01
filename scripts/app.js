ca31bbaf239ccd97f936984911dee0f7dec933bf
fajfara <anze@thefajfar.com>
2018-04-01 18:53:34 +0200

    Styling portfolio and adding new file structure

--- scripts/app.js
+++ scripts/app.js
@@ -85,42 +85,14 @@ $(document).ready(function(){
     };
 
     // Smooth scroll --> jsfiddle http://jsfiddle.net/cse_tushar/Dxtyu/141/
-    $('a[href^="#"]').on('click', function (e) {
+    $('#navbar a').click(function (e){
       e.preventDefault();
-      $(document).off("scroll");
-      
-      $('a').each(function () {
-          $(this).removeClass('active');
-      })
-      $(this).addClass('active');
-    
-      var target = this.hash,
-          menu = target;
-      $target = $(target);
-      $('html, body').stop().animate({
-          'scrollTop': $target.offset().top+20
-      }, 500, 'swing', function () {
-          window.location.hash = target;
-          $(document).on("scroll", onScroll);
-      });
+      var curLink = $(this);
+      var scrollPoint = $(curLink.attr('href')).position().top;
+      $('body, html').animate({
+        scrollTop: scrollPoint
+      }, 500);
     });
-
-    // On scroll method
-    function onScroll(event){
-      var scrollPos = $(document).scrollTop();
-      $('#menu-center a').each(function () {
-          var currLink = $(this);
-          var refElement = $(currLink.attr("href"));
-          if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-              $('#menu-center ul li a').removeClass("active");
-              currLink.addClass("active");
-          }
-          else{
-              currLink.removeClass("active");
-          }
-      });
-  }
-    
 
 
   });
